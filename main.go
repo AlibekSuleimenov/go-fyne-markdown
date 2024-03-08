1ed063621b7504198e4314205d742b1eb3bd3985
Alibek Suleimenov <alibek443@gmail.com>
2024-03-08 15:07:08 +0500

    added readme file, created desktop app for editing markdown

--- main.go
+++ main.go
@@ -1,8 +1,54 @@
 package main
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+// config represents the configuration settings for the application.
 type config struct {
+	EditWidget    *widget.Entry    // EditWidget holds the widget for editing text input.
+	PreviewWidget *widget.RichText // PreviewWidget holds the widget for displaying rich text preview.
+	CurrentFile   fyne.URI         // CurrentFile represents the URI of the currently opened file.
+	SaveMenuItem  *fyne.MenuItem   // SaveMenuItem holds the menu item for saving the current file.
 }
 
-func main() {
+// cfg represents the global configuration instance.
+var cfg config
 
+// Main function serves as the entry point for the application.
+// Application logic will be implemented here.
+func main() {
+	// create a fyne app
+	a := app.New()
+
+	// create window
+	win := a.NewWindow("Markdown")
+
+	// get user interface
+	edit, preview := cfg.makeUI()
+
+	// set the content of the window
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	// show window and run the app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+// makeUI creates and initializes the user interface components.
+// It returns pointers to the Entry widget for text input and the RichText widget for displaying rich text.
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+
+	edit.OnChanged = preview.ParseMarkdown
+
+	return edit, preview
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# **Desktop App with Go and Fyne**
+
+This is a simple desktop application written in Go using the Fyne toolkit. The application provides a user interface for performing specific tasks.
